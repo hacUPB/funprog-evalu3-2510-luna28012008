465ba36516af8daa1b207c16cdd66bd4287284ed
luna torres <luna.torresn@upb.edu.co>
2025-04-04 09:19:21 -0500

    ejercicios de introducion a punteros guardados

---
+++ ejercicio_punteros_funciones.c
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <math>
+
+int main(int argc, char const *argv[]){
+
+
+    return 0;
+}
+
+//funcion que recibe los tres numeros
+
+void numeros_promedio(dato1, dato2, dato3)
+{
+    float dato1, dato2, dato3;
+    float promedio;
+
+    printf("ingrese la nota #1: \n", dato1);
+    printf("ingrese la nota #2: \n", dato2);
+    printf("ingrese la nota #3: \n", dato3);
+
+    scanf("%f", &dato1);
+    scanf("%f", &dato2);
+    scanf("%f", &dato3);
+
+    promedio  = (dato1+dato2+dato3) /3
+
+}
